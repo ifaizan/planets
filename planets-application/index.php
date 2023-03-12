bbc6c8b530c8daad39108d73722f2f5a85606c73
Faizan Younus <50990523+ifaizan@users.noreply.github.com>
2023-03-12 20:56:49 +0100

    Update index.php

--- planets-application/index.php
+++ planets-application/index.php
@@ -5,7 +5,7 @@ echo '<!DOCTYPE html>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <style>
 		#solar-system {
-		   background: url("https://raw.githubusercontent.com/sidd-harth/planets/main/images/9.png")   center center;
+		   background: url("https://raw.githubusercontent.com/ifaizan/planets/main/planets-application/images/solar-system-9.png")   center center;
            background-repeat: no-repeat;			  
 		   background-size: cover;
 		   content: "";
@@ -23,7 +23,7 @@ echo '<!DOCTYPE html>
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			background: url("https://raw.githubusercontent.com/sidd-harth/planets/main/images/bg.png");
+			background: url("https://raw.githubusercontent.com/ifaizan/planets/main/planets-application/images/background.png");
 		}
 		
 /* 		.shadow {
